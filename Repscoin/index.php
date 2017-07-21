3c345618fd2a6a5d5dd30a0669015bb7e623a84c
GokuCraftHD <30347226+GokuCraftHD@users.noreply.github.com>
2017-07-21 15:44:07 +0200

    Actualización v0.1.1
    Mejoras en el diseño

---
+++ Repscoin/index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Repscoin</title>
+        <link rel="shortcut icon" type="image/x-icon" href="assets/img/repscoin.ico">
+        <meta charset="utf-8">
+        <meta name="description" content="">
+        <meta name="keywords" content="">
+        <meta name="author" content="GokuGamerHD">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        
+        <!-- Styles -->
+        <link rel="stylesheet" href="assets/css/bootstrap.css">
+        <link rel="stylesheet" href="assets/css/bootstrap-theme.css">
+        <style>
+        
+            .header {
+                display: flex;
+                justify-content: space-between;
+                padding: 15px;
+                background: #f1f1f1;
+            }
+                        
+            .header-title {
+                margin: 0;
+                font-size: 30px;
+            }
+        
+        </style>
+        
+        <!-- Scripts -->
+        <script src="http://www.azebac.es/jquery/jquery-3.2.1.js"></script>
+    </head>
+    <body>
+        <header class="header">
+            <div class="title-group" style=" display: flex; flex-wrap: wrap; ">
+                <img style=" width: 64px; height: 64px; " src="assets/img/repscoin.png" alt="repscoin" title="repscoin" class="logo">
+                <h1 style=" padding: 15px; " class="header-title"><span style=" color: #ff9800; ">R</span>epscoin</h1>
+            </div>
+            <nav class="menu" style=" padding: 15px; ">
+                <a href="#" class="btn btn-link">Start mining</a>
+                <a href="#" class="btn btn-link">News</a>
+                <a href="#" class="btn btn-link">Forum</a>
+            </nav>
+            <div class="buttons" style=" padding: 15px; ">
+                <div class="btn-group">
+                    <a href="#" class="btn btn-warning">Language</a>
+                    <a href="#" class="btn btn-warning dropdown-toggle" data-toggle="dropdown"><span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">French</a></li>
+                        <li><a href="#">Spanish</a></li>
+                        <li class="divider"></li>
+                        <li><a href="#">English</a></li>
+                    </ul>
+                </div>
+                <div class="btn-group sign-container">
+                    <a href="#" class="btn btn-warning">Sign-in</a>
+                    <a href="#" class="btn btn-warning">Log-in</a>
+                </div>
+            </div>
+        </header>
+        <script src="assets/js/bootstrap.js"></script>
+    </body>
+</html>
